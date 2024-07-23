b25b5fa64f0a5f5cc4d5d4503ffcbc1d11a4fcd9
apenfe <adrian1414@hotmail.es>
2024-07-23 19:04:33 +0200

    v-on

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
     <ClassBind></ClassBind>
+    <StyleBind></StyleBind>
 </template>
 
 <script lang="ts" setup>
 
 import ClassBind from './components/ClassBind.vue';
+import StyleBind from './components/StyleBind.vue';
 
 </script>
 
---
+++ src/components/StyleBind.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <p v-bind:style="{
+        backgroundColor: bg,
+        color: c
+    }">El mensaje se ha enviado satisfactoriamente</p>
+
+    <button v-on:click="handleClick()">Cambiar tema</button>
+
+</template>
+
+<script lang="ts" setup>
+  
+    import {ref} from 'vue';
+
+    let c = ref("white");
+    let bg = ref("blue");
+
+    function handleClick(){
+        c.value = 'white';
+        bg.value = 'black';
+    }
+
+</script>
+
+<style>
+
+</style>
