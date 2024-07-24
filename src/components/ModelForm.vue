f4308b423c32104789433f74f4ed0808ed337bcf
apenfe <adrian1414@hotmail.es>
2024-07-24 18:21:13 +0200

    Se explica bindeo bidireccional

--- src/components/ModelForm.vue
+++ src/components/ModelForm.vue
@@ -2,6 +2,12 @@
 
     <form>
         <input type="text" v-model="inputText"/>
+        <input type="checkbox" v-model="agree"/> Acepto términos y condiciones.
+        <select  v-model="country">
+            <option value="spain">Spain</option>
+            <option value="uk">UK</option>
+            <option value="france">France</option>
+        </select>
     </form>
 
     <h3>{{ inputText }}</h3>
@@ -13,6 +19,8 @@
     import {Ref, ref} from 'vue';
 
     let inputText:Ref<string> = ref('');
+    let agree:Ref<boolean> = ref(true);
+    let country:Ref<string> = ref('spain');
 
 </script>
 
